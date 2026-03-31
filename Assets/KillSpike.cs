4e3082f8b6da132ea61158a1d7709e4347646b1c
rokudaime <christianmomagainsttowa@gmail.com>
2026-03-31 09:24:15 +0200

    poziom better longer

---
+++ Assets/KillSpike.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class KillSpike : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        Destroy(collision.gameObject);
+    }
+}
